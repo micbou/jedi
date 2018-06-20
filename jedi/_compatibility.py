5a051df25b1e0fc1f0e5f64a0f6ab6d6cdd59e16
micbou <contact@micbou.com>
2018-06-20 14:55:04 +0200

    Use highest possible pickle protocol

--- jedi/_compatibility.py
+++ jedi/_compatibility.py
@@ -445,9 +445,6 @@ if sys.version_info[:2] == (3, 3):
     pickle.loads = loads
 
 
-_PICKLE_PROTOCOL = 2
-
-
 def pickle_load(file):
     try:
         if is_py3:
@@ -459,9 +456,9 @@ def pickle_load(file):
         raise EOFError()
 
 
-def pickle_dump(data, file):
+def pickle_dump(data, file, protocol):
     try:
-        pickle.dump(data, file, protocol=_PICKLE_PROTOCOL)
+        pickle.dump(data, file, protocol)
         # On Python 3.3 flush throws sometimes an error even though the writing
         # operation should be completed.
         file.flush()
@@ -469,6 +466,20 @@ def pickle_dump(data, file):
     # the correct type and error number.
     except OSError:
         raise IOError(errno.EPIPE, "Broken pipe")
+
+
+# Determine the highest protocol version compatible for a given list of Python
+# versions.
+def highest_pickle_protocol(python_versions):
+    protocol = 4
+    for version in python_versions:
+        if version[0] == 2:
+            # The minimum protocol version for the versions of Python that we
+            # support (2.7 and 3.3+) is 2.
+            return 2
+        if version[1] < 4:
+            protocol = min(protocol, 3)
+    return protocol
 
 
 try:
